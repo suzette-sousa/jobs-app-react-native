d04dc3130b3581a156bdbce3d950c4be24e19e4b
Suzette Sousa <sousa.suzette.pro@gmail.com>
2023-06-21 21:12:17 +0200

    head home screen

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello you !</Text>
+        <Text style={styles.welcomeMessage}>trouve ton job parfait</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
